9919382894012d3c8b8d727831a8cb77227af947
AyushSrivastava2793 <102364462+Ayush1403@users.noreply.github.com>
2023-10-20 02:28:02 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        bgmain: "#161A1D"
+      },
+      fontFamily:{
+        fontFamily: "Roboto Slab serif"
+      }
+    },
+  },
+  plugins: [],
+}
